7bb564db744dea0f705abb5397cbf0f3c8da0bee
unknown <divinlonji@gmail.com>
2019-08-04 04:53:44 +0200

    full calendar with some glitches on mvc

--- AdoptifySystem/AdoptifySystem/Controllers/HomeController.cs
+++ AdoptifySystem/AdoptifySystem/Controllers/HomeController.cs
@@ -8,23 +8,71 @@ namespace AdoptifySystem.Controllers
 {
     public class HomeController : Controller
     {
+        // GET: Home
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        public JsonResult GetEvents()
         {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
+            using (Wollies_ShelterEntities dc = new Wollies_ShelterEntities())
+            {
+                var events = dc.Event_Schedule.ToList();
+                return new JsonResult { Data = events, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            }
         }
 
-        public ActionResult Contact()
+        [HttpPost]
+        public JsonResult SaveEvent(Event_Schedule e)
         {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            var status = false;
+            using (Wollies_ShelterEntities dc = new Wollies_ShelterEntities())
+            {
+                if (e.EventID > 0)
+                {
+                    //Update the event
+                    var v = dc.Event_Schedule.Where(a => a.EventID == e.EventID).FirstOrDefault();
+                    if (v != null)
+                    {
+                        v.Subject = e.Subject;
+                        v.Start = e.Start;
+                        v.EventEnd = e.EventEnd;
+                        v.Description = e.Description;
+                        v.IsFullDay = e.IsFullDay;
+                        v.ThemeColour = e.ThemeColour;
+                    }
+                }
+                else
+                {
+                    dc.Event_Schedule.Add(e);
+                }
+                try
+                {
+                    dc.SaveChanges();
+                }
+                catch
+                { }
+                    status = true;
+                
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+        [HttpPost]
+        public JsonResult DeleteEvent(int eventID)
+        {
+            var status = false;
+            using (Wollies_ShelterEntities dc = new Wollies_ShelterEntities())
+            {
+                var v = dc.Event_Schedule.Where(a => a.EventID == eventID).FirstOrDefault();
+                if (v != null)
+                {
+                    dc.Event_Schedule.Remove(v);
+                    dc.SaveChanges();
+                    status = true;
+                }
+            }
+            return new JsonResult { Data = new { status = status } };
         }
     }
 }
