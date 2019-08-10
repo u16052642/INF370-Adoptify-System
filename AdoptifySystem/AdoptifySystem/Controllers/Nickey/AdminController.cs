f90590a256301deb9d1332bbcc2edc2998abdcd7
nickey <u17136319@tuks.co.za>
2019-08-10 11:31:33 +0200

    did check-in now going to do the checkout and reset password

--- AdoptifySystem/AdoptifySystem/Controllers/Nickey/AdminController.cs
+++ AdoptifySystem/AdoptifySystem/Controllers/Nickey/AdminController.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using Google.Authenticator;
 using AdoptifySystem.Models;
+using System.Net.Mail;
+using System.Net;
+using System.Web.Security;
 
 namespace AdoptifySystem.Controllers
 {
@@ -17,8 +19,16 @@ namespace AdoptifySystem.Controllers
 
         //this is the Db that i will be unstatnitatiung to use thought the whole controller
         Wollies_ShelterEntities1 db = new Wollies_ShelterEntities1();
+        public static Flexible flex = new Flexible();
         public ActionResult Login()
         {
+            var time = DateTime.Now;
+            //var nextime = DateTime.Now + 30; 
+            //while (time == )
+            //{
+            
+            //}
+            //ViewBag.Time = await time;
             return View();
         }
         [HttpPost]
@@ -32,13 +42,13 @@ namespace AdoptifySystem.Controllers
             List<User_> Users;
             try
             {
-                User_ user = new User_();
-                user.Username = "Divin";
-                user.Password = "food";
-                db.User_.Add(user);
-                db.SaveChanges();
+                //User_ user = new User_();
+                //user.Username = "Divin";
+                //user.Password = "food";
+                //db.User_.Add(user);
+                //db.SaveChanges();
                 Users = db.User_.ToList();
-                
+
             }
             catch (Exception e)
             {
@@ -82,10 +92,11 @@ namespace AdoptifySystem.Controllers
             ViewBag.Message = "Welcome " + Session["Username"].ToString();
             return View();
         }
+        [HttpPost]
         //here this is where we go and authorize the code that was genereated on the user mobile application
-        public ActionResult Verify2FA()
+        public ActionResult Verify2FA(string token)
         {
-            var token = Request["passcode"];
+            //var token = Request["passcode"];
             TwoFactorAuthenticator tfa = new TwoFactorAuthenticator();
             string UserUniqueKey = Session["UserUniqueKey"].ToString();
             bool isValid = tfa.ValidateTwoFactorPIN(UserUniqueKey, token);
@@ -104,9 +115,86 @@ namespace AdoptifySystem.Controllers
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult ResetPassword(string EmailID)
+        {
+            //Verify Email ID
+            //Generate Reset password link 
+            //Send Email 
+            string message = "";
+            bool status = false;
+
+            using (Wollies_ShelterEntities1 dc = new Wollies_ShelterEntities1())
+            {
+
+                //adding a employee
+                //Employee emp = new Employee();
+                //emp.Emp_Name = "jem";
+                //emp.Emp_Email = "jemimakola99@gmail.com";
+                //dc.Employees.Add(emp);
+                //dc.SaveChanges();
+
+                //User_ useremp = new User_();
+                //useremp.Emp_ID = 1;
+                //useremp.Username = "nick";
+                //useremp.Password = "john";
+                //dc.User_.Add(useremp);
+                //dc.SaveChanges();
+                var account = dc.Employees.Where(a => a.Emp_Email == EmailID).FirstOrDefault();
+                var user = dc.User_.Where(a => a.Emp_ID == account.Emp_ID).FirstOrDefault();
+                if (user != null)
+                {
+                    //Send email for reset password
+                    string resetCode = Membership.GeneratePassword(12, 1);
+                    SendVerificationLinkEmail(account.Emp_Email, resetCode, "ResetPassword");
+                    user.Password = resetCode;
+                    //This line I have added here to avoid confirm password not match issue , as we had added a confirm password property 
+                    //in our model class in part 1
+                    dc.Configuration.ValidateOnSaveEnabled = false;
+                    dc.SaveChanges();
+                    message = "Reset password link has been sent to your email id.";
+                }
+                else
+                {
+                    message = "Account not found";
+                }
+            }
+            ViewBag.Message = message;
+            return View();
+        }
         public ActionResult Checkin()
         {
+
+            //Employee emp = new Employee();
+            //emp.Emp_Name = "nick";
+            //emp.Emp_Surname = "Sibanyoni";
+            //db.Employees.Add(emp);
+            //Employee emp1 = new Employee();
+            //emp1.Emp_Name = "james";
+            //emp1.Emp_Surname = "High";
+            //db.Employees.Add(emp1);
+            //Employee emp2 = new Employee();
+            //emp2.Emp_Name = "Mercury";
+            //emp2.Emp_Surname = "Freddy";
+            //db.Employees.Add(emp2);
+            //db.SaveChanges();
+            
+            flex.employeelist = db.Employees.ToList();
+            return View(flex);
+        }
+        [HttpPost]
+        public ActionResult Checkin(int id)
+        {
             return View();
+        }
+        [HttpPost]
+        public ActionResult GetUserDetails()
+        {
+            int userId = Convert.ToInt32(Request.Form["id"]);
+            //fetch the data by userId and assign in a variable, for ex: myUser
+            //Flexible myUser = new Flexible();
+                flex.employee = db.Employees.Find(userId);
+            return View("Checkin", flex);
         }
         public ActionResult Checkout()
         {
@@ -172,5 +260,43 @@ namespace AdoptifySystem.Controllers
         {
             return View();
         }
+
+        [NonAction]
+        public void SendVerificationLinkEmail(string emailID, string activationCode, string emailFor = "VerifyAccount")
+        {
+           using (MailMessage mail = new MailMessage())
+            {
+                mail.From = new MailAddress("u17136319@tuks.co.za");
+                mail.To.Add(emailID);
+                mail.Subject = "Hello World";
+                mail.Body = "<h1>Hello Jem</h1><br><br><p>please find your code attached</p>";
+                mail.IsBodyHtml = true;
+                //mail.Attachments.Add(new Attachment("C:\\file.zip"));
+
+                using (SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587))
+                {
+                    smtp.Credentials = new NetworkCredential("u17136319@tuks.co.za", "Urahara123");
+                    smtp.EnableSsl = true;
+                    smtp.Send(mail);
+                }
+            }
+                //var smtp = new SmtpClient
+                //{
+                //    Host = "smtp.gmail.com",
+                //    Port = 587,
+                //    EnableSsl = true,
+                //    DeliveryMethod = SmtpDeliveryMethod.Network,
+                //    UseDefaultCredentials = false,
+                //    Credentials = new NetworkCredential(fromEmail.Address, fromEmailPassword)
+                //};
+
+                //using (var message = new MailMessage(fromEmail, toEmail)
+                //{
+                //    Subject = subject,
+                //    Body = body,
+                //    IsBodyHtml = true
+                //})
+                //    smtp.Send(message);
+            }
     }
 }
